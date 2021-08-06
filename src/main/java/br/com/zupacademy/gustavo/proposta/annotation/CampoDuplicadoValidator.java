e9b6ae824d92fd599fa7587f155a2033e0479b96
GustavoMede <gustavomedeiros0708@outlook.com>
2021-08-06 13:13:56 -0300

    fix: validação de documento duplicado encriptado

--- src/main/java/br/com/zupacademy/gustavo/proposta/annotation/CampoDuplicadoValidator.java
+++ src/main/java/br/com/zupacademy/gustavo/proposta/annotation/CampoDuplicadoValidator.java
@@ -1,10 +1,19 @@
 package br.com.zupacademy.gustavo.proposta.annotation;
 
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.HttpStatusCodeException;
+
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class CampoDuplicadoValidator implements ConstraintValidator<CampoDuplicado, Object> {
@@ -24,6 +33,24 @@ public class CampoDuplicadoValidator implements ConstraintValidator<CampoDuplica
 
     @Override
     public boolean isValid(Object value, ConstraintValidatorContext constraintValidatorContext) {
+        if(nomeCampo.equals("documento")){
+            List<Boolean> valida = new ArrayList<>();
+            System.out.println("Entrando na validação");
+            Query query = entityManager.createQuery("Select documento from "+ nomeClasse.getName());
+            List<?> resultadoConsulta = query.getResultList();
+            resultadoConsulta.forEach(resultado -> {
+                 valida.addAll(Collections.singletonList(new BCryptPasswordEncoder().matches(value.toString(), resultado.toString())));
+            });
+            for(int i = 0; i < valida.size(); i++){
+                if(valida.contains(true)){
+                    throw HttpClientErrorException.UnprocessableEntity.create(HttpStatus.UNPROCESSABLE_ENTITY,
+                            "Proposta já realizada para esse documento.", HttpHeaders.EMPTY,
+                            null, null);
+                }
+            }
+            return true;
+        }
+        System.out.println("Entrando na validação.");
         Query query = entityManager.createQuery("Select 1 from "+ nomeClasse.getName() +" where "+ nomeCampo +"=:value");
         query.setParameter("value", value);
         List<?> resultadoConsulta = query.getResultList();
--- src/main/java/br/com/zupacademy/gustavo/proposta/exceptionHandler/ExceptionsHandler.java
+++ src/main/java/br/com/zupacademy/gustavo/proposta/exceptionHandler/ExceptionsHandler.java
@@ -4,15 +4,23 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @RestControllerAdvice
-public class ExceptionHandler extends ResponseEntityExceptionHandler {
+public class ExceptionsHandler extends ResponseEntityExceptionHandler {
 
     @Override
     protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+
+        return new ResponseEntity<>(new HttpHeaders(), HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(value = HttpClientErrorException.UnprocessableEntity.class)
+    protected ResponseEntity<Object> handleUnprocessableEntity(HttpClientErrorException.UnprocessableEntity ex){
 
         return new ResponseEntity<>(new HttpHeaders(), HttpStatus.UNPROCESSABLE_ENTITY);
     }
