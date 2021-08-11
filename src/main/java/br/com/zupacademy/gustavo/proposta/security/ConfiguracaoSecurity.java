e9317e62a00f9559a901bafbe92556607e7f289f
GustavoMede <gustavomedeiros0708@outlook.com>
2021-08-11 12:30:04 -0300

    fix: integracao-prometheus-keycloak

--- src/main/java/br/com/zupacademy/gustavo/proposta/security/ConfiguracaoSecurity.java
+++ src/main/java/br/com/zupacademy/gustavo/proposta/security/ConfiguracaoSecurity.java
@@ -17,7 +17,7 @@ public class ConfiguracaoSecurity extends WebSecurityConfigurerAdapter {
                         .antMatchers(HttpMethod.POST, "/proposta/**").hasAuthority("SCOPE_proposta:write")
                         .antMatchers(HttpMethod.POST, "/bloqueio/**").hasAuthority("SCOPE_proposta:write")
                         .antMatchers(HttpMethod.POST, "/aviso/**").hasAuthority("SCOPE_proposta:write")
-                        .antMatchers(HttpMethod.GET, "/actuator/**").hasAuthority("SCOPE_proposta:read")
+                        .antMatchers(HttpMethod.GET, "/actuator/prometheus").hasAuthority("SCOPE_prometheus")
                         .anyRequest().authenticated()
         ).oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt);
     }
